37e70db2462f5f84acc3d248f6d73a641c182383
CHRISTOPHER QUIOCHO <CHRISTOPHER.QUIOCHO@GMAIL.COM>
2020-08-26 11:37:08 -0700

    Added major story content

--- js/narrative.js
+++ js/narrative.js
@@ -6,4 +6,27 @@
 // could also render the story in javascript so it takes in the players name to return 'Sara, you wake up in the Gryffindor dormatory to the sound of moaning myrtle...' or whatever.
 
 // retrieves user info at newPlayer.key
-let newPlayer = JSON.parse(localStorage.getItem('userInfo'));
+newPlayer = JSON.parse(localStorage.getItem('userInfo'));
+let narrativeForm = document.getElementById('narrative');
+let name = newPlayer.userName;
+let house = newPlayer.hogwartsHouse;
+
+function renderStory() {
+    let h2element = document.createElement('h2');
+    h2element.textContent = `Good morning ${name}!`;
+    narrativeForm.appendChild(h2element);
+    let brElement = document.createElement('br');
+    let pElement = document.createElement('p');
+    pElement.textContent = `You are abruptly woken on the morning of your final exam in the ${house} dormitory by Moaning Myrtle. She obnoxiously informs you that you urgently needed to retrieve your friend Esmeralda from the second-floor girls\’ lavatory. As Myrtle floats away, you hear her mutter \“I want that swine out of my bathroom…\”.`
+    narrativeForm.appendChild(pElement);
+    narrativeForm.appendChild(brElement);
+}
+
+function continueStory(event) {
+    event.preventDefault();
+    window.location.replace("../pages/lavatory.html");
+  }
+  
+  listener.addEventListener('submit', continueStory);
+
+  renderStory();
